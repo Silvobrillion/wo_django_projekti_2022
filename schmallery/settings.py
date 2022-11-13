42ea9db25f65dd6fd5e0712991ced0481c12fb06
Izabel Silvola <izabel.silvola@student.hamk.fi>
2022-11-13 23:56:37 +0200

    secret key piilotettu

--- schmallery/settings.py
+++ schmallery/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--aal*%9!kj+38(1i^#y2vwbix7dw!@)6y=2@hjr5oal$94_h@+'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
